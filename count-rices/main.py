d604415b344fe63a12e0fe37a000486054742cf4
Otávio Thomas Bertucini <otaviobertucini@gmail.com>
2021-11-22 19:49:03 -0300

    .

--- count-rices/main.py
+++ count-rices/main.py
@@ -12,7 +12,7 @@ import cv2
 
 #===============================================================================
 
-INPUT_IMAGE =  './images/150.bmp'
+INPUT_IMAGE =  './images/205.bmp'
 
 NEGATIVO = False
 THRESHOLD = 0.60
@@ -24,35 +24,14 @@ sys.setrecursionlimit(1500)
 
 #===============================================================================
 
-def binariza (img, threshold):
-    ''' Binarização simples por limiarização.
 
-Parâmetros: img: imagem de entrada. Se tiver mais que 1 canal, binariza cada
-              canal independentemente.
-            threshold: limiar.
-            
-Valor de retorno: versão binarizada da img_in.'''
+def binariza(img, threshold):
 
-    return np.where(img < THRESHOLD, 0, 1)
+    return 255 - cv2.adaptiveThreshold(255 - img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 15, 3)
 
 #-------------------------------------------------------------------------------
 
 def rotula (img, largura_min, altura_min, n_pixels_min):
-    '''Rotulagem usando flood fill. Marca os objetos da imagem com os valores
-[0.1,0.2,etc].
-
-Parâmetros: img: imagem de entrada E saída.
-            largura_min: descarta componentes com largura menor que esta.
-            altura_min: descarta componentes com altura menor que esta.
-            n_pixels_min: descarta componentes com menos pixels que isso.
-
-Valor de retorno: uma lista, onde cada item é um vetor associativo (dictionary)
-com os seguintes campos:
-
-'label': rótulo do componente.
-'n_pixels': número de pixels do componente.
-'T', 'L', 'B', 'R': coordenadas do retângulo envolvente de um componente conexo,
-respectivamente: topo, esquerda, baixo e direita.'''
 
     # Use a abordagem com flood fill recursivo.
 
@@ -108,8 +87,6 @@ respectivamente: topo, esquerda, baixo e direita.'''
                 flood(x, y, component)
                 components.append(component)
                 component_count += 1
-    
-    print(components)        
 
     return list(filter(lambda component: component['n_pixels'] > n_pixels_min, components))
 
@@ -118,38 +95,71 @@ respectivamente: topo, esquerda, baixo e direita.'''
 def main ():
 
     # Abre a imagem em escala de cinza.
-    img = cv2.imread (INPUT_IMAGE, cv2.IMREAD_GRAYSCALE)
+    original = cv2.imread (INPUT_IMAGE, cv2.IMREAD_COLOR)
+    img = original.copy()
     if img is None:
         print ('Erro abrindo a imagem.\n')
         sys.exit ()
 
     # É uma boa prática manter o shape com 3 valores, independente da imagem ser
     # colorida ou não. Também já convertemos para float32.
-    img = img.reshape ((img.shape [0], img.shape [1], 1))
-    img = img.astype (np.float32) / 255
 
+    img = cv2.cvtColor (img, cv2.COLOR_BGR2GRAY)
+    img = img.reshape ((img.shape [0], img.shape [1], 1))
     # Mantém uma cópia colorida para desenhar a saída.
-    img_out = cv2.cvtColor (img, cv2.COLOR_GRAY2BGR)
+    img_out = img
+
 
     # Segmenta a imagem.
     if NEGATIVO:
-        img = 1 - img
+        img = 1 - img    
+
+    ## binariza via limiar adaptativo
     img = binariza (img, THRESHOLD)
-    # cv2.imshow ('01 - binarizada', img)
-    cv2.imwrite ('01 - binarizada.png', img*255)
 
-    start_time = timeit.default_timer ()
-    componentes = rotula (img, LARGURA_MIN, ALTURA_MIN, N_PIXELS_MIN)
-    n_componentes = len (componentes)
-    print ('Tempo: %f' % (timeit.default_timer () - start_time))
-    print ('%d componentes detectados.' % n_componentes)
+    ## tirar ruído via abertura (morfologia)
+    kernel = np.ones((6,6),np.uint8)
+    img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+    img = img.reshape ((img.shape [0], img.shape [1], 1))
+    cv2.imwrite ('01 - binarizada.png', img)
 
-    # Mostra os objetos encontrados.
-    for c in componentes:
-        cv2.rectangle (img_out, (c ['L'], c ['T']), (c ['R'], c ['B']), (0,0,1))
+    kernel = np.ones((3,3),np.uint8)
+    img_erosion = cv2.erode(img, kernel, iterations=2)
+    cv2.imwrite ('01 - erodida.png', img_erosion)
 
-    # cv2.imshow ('02 - out', img_out)
-    cv2.imwrite ('02 - out.png', img_out*255)
+    duo = np.zeros(img.shape)
+
+    for y in range(0, img.shape[0]):
+        for x in range(0, img.shape[1]):
+            if(img[y][x][0] == 255):
+                duo[y][x][0] = original[y][x][0]
+
+    cv2.imwrite ('01 - duo.png', duo)
+
+    a,ret = cv2.threshold(duo,200,255,cv2.THRESH_BINARY)
+
+    kernel = np.ones((5,5),np.uint8)
+    ret = cv2.erode(ret, kernel, iterations=1)
+
+    cv2.imwrite ('01 - ret.png', ret)
+
+# 
+    # output = np.zeros(original.shape)
+    # output = cv2.normalize(duo, output, 0, 255, cv2.NORM_MINMAX)
+    # cv2.imwrite ('01 - normalized.png', duo)
+
+    # start_time = timeit.default_timer ()
+    # componentes = rotula (img, LARGURA_MIN, ALTURA_MIN, N_PIXELS_MIN)
+    # n_componentes = len (componentes)
+    # print ('Tempo: %f' % (timeit.default_timer () - start_time))
+    # print ('%d componentes detectados.' % n_componentes)
+
+    # # Mostra os objetos encontrados.
+    # for c in componentes:
+    #     cv2.rectangle (img_out, (c ['L'], c ['T']), (c ['R'], c ['B']), (0,0,1))
+
+    # # cv2.imshow ('02 - out', img_out)
+    # cv2.imwrite ('02 - out.png', img_out*255)
     cv2.waitKey ()
     cv2.destroyAllWindows ()
 
