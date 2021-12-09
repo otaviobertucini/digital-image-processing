f6fb3c2f956a55c065615b96122a09f05030f299
Otávio <otavio@cargon.com.br>
2021-12-09 11:42:45 -0300

    .

--- chroma-key/main.py
+++ chroma-key/main.py
@@ -1,15 +1,13 @@
-## Alunos: Otávio Bertucini e Isadora Chandelier
+# Alunos: Otávio Bertucini e Isadora Chandelier
 
 import sys
-import timeit
 import numpy as np
 import cv2
-from matplotlib.pyplot import boxplot
-import matplotlib.pyplot as plt
 import math
 
 INPUT_IMAGE = './images/0.bmp'
 BACK_IMAGE = './back.bmp'
+
 GREEN = [0, 255, 0]
 R = 0
 G = 1
@@ -17,20 +15,22 @@ B = 2
 
 GAMMA = 2
 
-## Correção Gama, baseado em https://docs.opencv.org/3.4/d3/dc1/tutorial_basic_linear_transform.html
+THRESHOLD = 20
+
+# Correção Gama, baseado em https://docs.opencv.org/3.4/d3/dc1/tutorial_basic_linear_transform.html
 def contrast(img):
 
-    img = img.astype (np.uint8)
+    img = img.astype(np.uint8)
 
-    lookUpTable = np.empty((1,256), np.uint8)
+    lookUpTable = np.empty((1, 256), np.uint8)
     for i in range(256):
-        lookUpTable[0,i] = np.clip(pow(i / 255.0, GAMMA) * 255, 0, 255)
+        lookUpTable[0, i] = np.clip(pow(i / 255.0, GAMMA) * 255, 0, 255)
 
     new_image = cv2.LUT(img, lookUpTable)
 
     return new_image
 
-## metodo baseado em https://www.compuphase.com/cmetric.htm
+# metodo baseado em https://www.compuphase.com/cmetric.htm
 def colorDelta(color):
 
     red = (color[R] + GREEN[R]) / 2
@@ -39,20 +39,26 @@ def colorDelta(color):
     delta_blue = color[B] - GREEN[B]
 
     delta = math.sqrt(((2 + (red / 255)) * delta_red**2) + (4 * delta_green ** 2) + ((2 + (255 - red) / 255) * delta_blue ** 2))
+    # delta = math.sqrt((delta_red**2) + (delta_green**2) + (delta_blue**2))
 
     return delta
+
 
 def generateMatte(img):
 
     matte = np.zeros((img.shape[0], img.shape[1]))
 
-    for x in range(0, img.shape[0]):
-        for y in range(0, img.shape[1]):
-            color = img[x][y]
-            delta = colorDelta(color)
-            matte[x][y] = delta
+    # img_lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
 
-    return matte * 255
+    for y in range(0, img.shape[0]):
+        for x in range(0, img.shape[1]):
+            color = img[y][x]
+            # print(color)
+            delta = colorDelta(color)
+            matte[y][x] = delta
+
+    return matte
+
 
 def removeBackground(img, back, matte):
 
@@ -60,39 +66,44 @@ def removeBackground(img, back, matte):
 
     for y in range(img.shape[0]):
         for x in range(img.shape[1]):
-            # print(matte[y][x])
-            # if(np.trunc(matte[y][x]) == 0):
-            #     result[y][x] = back[y][x]
-            #     continue
-            # print(matte[y][x])
-            if(matte[y][x] > 45):
+            if(matte[y][x] > THRESHOLD):
                 result[y][x] = img[y][x]
                 continue
-            # result[y][x] = (matte[y][x]) * back[y][x]
-    
+            result[y][x] = ( 1 - (matte[y][x] / 255)) * back[y][x]
+
     print(result.shape)
     return result
 
-def main ():
+
+def main():
 
     # Abre a imagem em escala de cinza.
-    original = cv2.imread (INPUT_IMAGE, cv2.IMREAD_COLOR)
+    original = cv2.imread(INPUT_IMAGE, cv2.IMREAD_COLOR)
+    cv2.imwrite('original.bmp', original)
     img = cv2.cvtColor(original, cv2.COLOR_BGR2RGB)
     if img is None:
-        print ('Erro abrindo a imagem.\n')
-        sys.exit ()
+        print('Erro abrindo a imagem.\n')
+        sys.exit()
 
-    back = cv2.imread (BACK_IMAGE, cv2.IMREAD_COLOR)
+    back = cv2.imread(BACK_IMAGE, cv2.IMREAD_COLOR)
     if back is None:
-        print ('Erro abrindo o back.\n')
-        sys.exit ()
+        print('Erro abrindo o back.\n')
+        sys.exit()
 
-    resized = cv2.resize(back, (original.shape[1], original.shape[0]), interpolation= cv2.INTER_LINEAR)
+    resized = cv2.resize(
+        back, (original.shape[1], original.shape[0]), interpolation=cv2.INTER_LINEAR)
     cv2.imwrite('resized.bmp', resized)
 
-    matte = generateMatte(img)
+    matte = generateMatte(original)
     matte = cv2.normalize(matte, matte, 0, 1, cv2.NORM_MINMAX)
     matte = matte * 255
+    
+    kernel = np.ones((4,4),np.uint8)
+    matte = cv2.morphologyEx(matte, cv2.MORPH_OPEN, kernel)
+
+    matte = cv2.normalize(matte, matte, 0, 1, cv2.NORM_MINMAX)
+    matte = matte * 255
+    # equ = cv2.equalizeHist(matte)
     # print(matte)
     cv2.imwrite('matte.bmp', matte)
 
@@ -105,6 +116,6 @@ def main ():
 
 
 if __name__ == '__main__':
-    main ()
+    main()
 
-#===============================================================================
+# ===============================================================================
